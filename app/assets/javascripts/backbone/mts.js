25149364d1781c6ca139333829d59cd1ebc6e05c
Kasabian <pandascorp@mail.ru>
2013-04-17 19:52:16 +0300

    rails bootstrap

--- app/assets/javascripts/backbone/mts.js
+++ app/assets/javascripts/backbone/mts.js
@@ -1,14 +1,13 @@
 
 //= require_tree ./templates
 
-//= require ./models/cat_model
+
 //= require ./models/spec_model
 
-//= require ./collections/cat_collection
+
 //= require ./collections/spec_collection
 
-//= require ./views/cat_view
-//= require ./views/cat_collection_view
+
 //= require ./views/spec_view
 //= require ./views/spec_collection_view
 
