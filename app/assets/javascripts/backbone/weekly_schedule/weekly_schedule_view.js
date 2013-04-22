1da2aa0c5151e03848327b938b25765cf87718b8
pavshulin <pavshulin@gmail.com>
2013-04-22 09:18:47 +0300

    update weekly front-end and select daily schedule

--- app/assets/javascripts/backbone/weekly_schedule/weekly_schedule_view.js
+++ app/assets/javascripts/backbone/weekly_schedule/weekly_schedule_view.js
@@ -5,7 +5,11 @@
         tagName : 'tr',
         
         initialize : function(){
-
+            
+        },
+        
+        events : {
+            'click .schedule-item' : 'selectItem'
         },
         
         render : function(){
@@ -18,14 +22,67 @@
                 this.$el.append($('<td />', 
                     {
                        text : schedule[i].start + ' - ' + schedule[i].end,
-                       id : this.model.get('doctor_id') + '-' + i
+                       id : this.model.get('doctor_name') + '-' + i,
+                       "class" : 'schedule-item'
                     }
                 
                 ));            
            }
             
             return this;
+        },
+        
+        selectItem : function(e){
+            
+            var target = e.target,
+            attr_data = $(target).attr('id').split('-');
+            
+            if(attr_data.length === 2){
+                
+                if(this.triggerSelect($(target))){
+                    
+                    Backbone.Mediator.pub('weekly_selectItem', 
+                        {
+                            name : this.model.get('doctor_name'),
+                            id : this.model.get('doctor_id'),
+                            duration : this.model.get('doctor_duration'),
+                            schedule : $(target).text()
+                        }
+                    );
+                    
+                }else{
+                    
+                    Backbone.Mediator.pub('weekly_unselectItem', 
+                        {
+                            name : this.model.get('name'),
+                            id : this.model.get('id'),                            
+                        }
+                    );
+                    
+                }
+                
+            }else{
+                console.warn('wrong id or selected element');
+            }
+            
+        },
+        
+        triggerSelect : function(elem) {
+            
+            var result = false;
+            
+            if(elem.hasClass('active')){
+                elem.removeClass('active');
+                result = false;
+            }else{
+                elem.addClass('active');
+                result = true;
+            }
+            
+            return result;
+            
         }
+        
     });
     
     
--- app/assets/javascripts/backbone/weekly_schedule/weekly_schedules_view.js
+++ app/assets/javascripts/backbone/weekly_schedule/weekly_schedules_view.js
@@ -4,26 +4,36 @@
         
         //el : $('#weekly-table'),
         
+        views : {},
+        
         collect: new WeeklyCollection(),
         
         initialize : function(){
-
+            
+            this.renderDate ();
+            
             this.collect.fetch();
             
             Backbone.Mediator.sub('doctor_selected', this.addSchedule, this);
+            Backbone.Mediator.sub('doctor_unselected', this.removeSchedule, this)
+
         },
 
         
         addSchedule : function(data){
+
             
             var model = this.collect.where({doctor_id : data.id})[0],
             view = {};
+            
             if(model){
             
-                model.set({'doctor_name' : data.name})
-            
+                model.set({'doctor_name' : data.name});
+                model.set({'doctor_duration' : data.duration});
+                
                 view = new WeeklyView({model : model});
-                this.render(view)
+                this.render(view);
+                this.views[model.get('doctor_id')] = view;
                 
             }else{
                 console.warn("this doctor don't have schedule list ");
@@ -32,15 +42,57 @@
             
         },
         
+        renderDate : function(){
+            
+            var date = new Date(),
+            day = date.getDay(),
+            current_date = date.getDate(),
+            i=0
+            ;
+            
+            for(i;i<7;i++){
+                date.setDate(current_date+(i-day));
+                this.renderDay(date)
+            }
+            
+        },
+        
+        renderDay : function(date) {
+           var elem = this.$el.find('thead tr'),
+           days = ['Sunday', 'Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']
+           ;
+           
+           elem.append($('<td>', 
+            
+            {
+                id : days[date.getDay()],
+                text : days[date.getDay()] + ' ' + date.getDate() + ' ' + (date.getMonth()+1) + ' ' + date.getFullYear()
+                 
+            }));
+
+        },
+        
         render : function(view){
             
             this.$el.show()
             this.$el.append(view.render().el)
             
             return this;
-        }
+        },
         
-        //removeSchedule
+        removeSchedule : function(data) {
+            
+            var view = this.views[data.id];
+            
+            if(view){
+                
+                view.remove();
+                delete this.views[data.id];
+                
+            }else{
+                console.warn('schedule list for this doctor is not exist')
+            }
+        }
         
     });
     
