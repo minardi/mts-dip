0f7dc56b294f6ce05a50ce7a62351e4e553b70f9
inhuman-en <inhuman.en@gmail.com>
2013-04-23 22:10:22 +0300

    daily schedule completed

--- app/assets/javascripts/backbone/daily_schedule/daily_schedules_view.js
+++ app/assets/javascripts/backbone/daily_schedule/daily_schedules_view.js
@@ -4,56 +4,71 @@
 
 			el: $("#daily_schedules"), 
 			template: JST["backbone/daily_schedule/daily_schedules_template"],
-
-			views: {},
 						
 			initialize: function() {
 				this.daily_schedules = new app.DailySchedules();
 
 				Backbone.Mediator.sub("weekly_selectItem", this.addDailySchedule, this);
 				Backbone.Mediator.sub("weekly_unselectItem", this.removeDailySchedule, this);
-				this.render();
+				this.render();			
 			},
 
 			addDailySchedule: function(attr) {
 
-				var schedule_array = attr["schedule"].split(" - ");
-					schedule_start = schedule_array[0];
+				var schedule_array = attr["schedule"].split(" - "),
+					schedule_start = schedule_array[0],
 					schedule_end = schedule_array[1];
+					
+					/*day = "";
+
+				switch (attr["day"]){
+					case "sun":
+						day = "Sunday";
+					break;
+					case "mon":
+						day = "Monday";
+					break;
+					case "tue":
+						day = "Tuesday";
+					break;
+					case "wed":
+						day = "Wednesday";
+					break;
+					case "thu":
+						day = "Thursday";
+					break;
+					case "fri":
+						day = "Friday";
+					break;
+					case "sat":
+						day = "Saturday";
+					break;					
+				}*/
 				
 				daily_schedule = new app.DailySchedule( { doctor_id: attr["id"],
 													      doctor_name: attr["name"],
+													      day: attr["day"],
 													      //duration: attr["duration"],
 													      schedule_start: schedule_start,
-													      schedule_end: schedule_end
+													      schedule_end: schedule_end,
+													      visible: true
 												      } );
-
-				console.log(daily_schedule);
 
 				this.daily_schedules.add(daily_schedule);
 
 				daily_schedule_view = new app.DailyScheduleView( {model: daily_schedule} );
-
-				//добавить дату
-				this.views[daily_schedule.get("doctor_id")] = daily_schedule_view;
 
 				this.$el.find("#daily_schedules_content").append(daily_schedule_view.render().el);
 			},
 
 			removeDailySchedule: function(attr) {
 
-				//определяем удаляемый daily schedule по дате и доктору (добавить дату)
 				var daily_schedule_to_remove = this.daily_schedules.where({ 
-																			doctor_id: attr["id"]
+																			doctor_id: attr["id"],
+																			day: attr["day"]
                     													  });
+				daily_schedule_to_remove[0].set("visible", false);
 				this.daily_schedules.remove(daily_schedule_to_remove);
-				console.log(this.views);
-				var view_to_remove = this.views[attr["id"]];
-            
-            	if(view_to_remove) {
-                	view_to_remove.remove();
-                	delete this.views[attr["id"]];
-                }
 			},
 
 			render: function() {		
