81f94082656a6f5b272abb12be5dcfd453a119f1
inhuman-en <inhuman.en@gmail.com>
2013-04-29 01:20:25 +0300

    doctor's schedule for current week

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,7 +4,8 @@ $(function () {
   var specializationList = new SpecsView({el:$("#specializations")}),
   	  doctorsView = new DoctorsView({el:$(".doctors")}),
   	  dayTimelines = new DailySchedulesView({el:$("#daily_schedules")}),
-          ticketsView = new TicketsView(),
-          weekly = new WeeklyCollectionView({el : $('#weekly-table')});  	
+  	  currentTimelines = new CurrentSchedulesView({el:$("#current_schedules")}),
+      ticketsView = new TicketsView(),
+      weekly = new WeeklyCollectionView({el : $('#weekly-table')});  	
  
 });
