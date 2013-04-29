b146a8e1f4a03b9147eb11591422dd8b33ebf6e9
LinnykS <Linnyk_Svetlana@i.ua>
2013-04-29 12:27:23 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ MTSDip::Application.routes.draw do
 
   resources :specializations
 
-  #resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
-  #match '/signup',  to: 'users#new'
-  #match '/signin',  to: 'sessions#new'
-  #match '/signout', to: 'sessions#destroy', via: :delete
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   get "main/home"
 
