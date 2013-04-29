7acac557dc8c9991bea78f36327ac6f6b100ca45
Svetlana <Linnyk_Svetlana@i.ua>
2013-04-29 12:07:55 +0300

    User DataBase

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,15 +4,17 @@ class SessionController < ApplicationController
   end
 
   def create
-     user = User.find_by_email(params[:session][:email].downcase)
+    user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
+      sign_in user
     else
       flash[:error] = 'Invalid email/password combination' # Not quite right!
-    render 'new'
+      render 'new'
+    end
   end
 
   def destroy
-
+    sign_out
+    redirect_to root_url
   end
 end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,36 @@
 module SessionHelper
+  #Если пользователь вошел и определен, присваеваем ему
+  #уникальный ид для работы с сессиями
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  #Текущий пользователь (для входа)
+  def current_user=(user)
+    @current_user = user
+  end
+
+  #Пользователь активный, то есть вошел в систему
+  def signed_in?
+    !current_user.nil?
+  end
+
+  #Поиск пользователя (в случае перехода на другую страницу)
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+   #Пользователь вышел
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
 end
