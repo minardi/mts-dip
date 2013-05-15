3690029b3c5e9ff1ea3879224071bf65e390b842
Glukman <PSGlukman@mail.ru>
2013-05-15 14:47:29 +0300

    Fix

--- app/assets/javascripts/backbone/tickets/next_ticket_view.js
+++ app/assets/javascripts/backbone/tickets/next_ticket_view.js
@@ -3,7 +3,7 @@
     app.NextTicketView = Backbone.View.extend({
 
         tagName: "ul",
-        className: "next-tickets",
+        className: "drop_menu_block",
 
         template: JST ["backbone/tickets/next_ticket_template"],
 
