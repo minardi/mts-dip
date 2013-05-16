70fb9fae87248b19ee106398bb2d2adf2a7c0eac
inhuman-en <inhuman.en@gmail.com>
2013-05-16 20:20:46 +0300

    DateEx mod

--- app/assets/javascripts/helpers/dateex.js
+++ app/assets/javascripts/helpers/dateex.js
@@ -1,97 +1,75 @@
-function DateEx() {
+function DateEx(dateobj) {
 
-	this.date = null;
+	this.date = dateobj || new Date();
+
+	function addZero(num) {
+
+		if (num < 10) {
+			num = '0' + num;
+		}
+
+		return num;
+	}
 
 	this.setDate = function(year, month, day, hours, minutes, seconds, ms) {
 
 		this.date = new Date(year, month, day, hours, minutes, seconds, ms);
 	}
 
-	this.dateTranspFormat = function() {
+	this.dateTransFormat = function() {
 
-		var dd = this.date.getDate(),
-			mm = this.date.getMonth() + 1,
-			yyyy = this.date.getFullYear();
-
-		if (dd < 10) {
-			dd = '0' + dd;
-		}
-
-		if (mm < 10) {
-			mm = '0' + mm;
-		}	
+		var dd = addZero(this.date.getDate()),
+			mm = addZero(this.date.getMonth() + 1),
+			yyyy = addZero(this.date.getFullYear());
 
 		return dd + "-" + mm + "-" + yyyy;
 	}
 
-	this.timeTranspFormat = function() {
+	this.timeTransFormat = function() {
 
-		var hh = this.date.getHours(),
-			mm = this.date.getMinutes();
-
-		if (hh < 10) {
-			hh = '0' + hh;
-		}
-
-		if (mm < 10) {
-			mm = '0' + mm;
-		}	
+		var hh = addZero(this.date.getHours()),
+			mm = addZero(this.date.getMinutes());
 
 		return "t" + hh + mm;
 	}
 
 	this.dateViewFormat = function() {
 
-		var dd = this.date.getDate(),
-			mm = this.date.getMonth() + 1,
-			yyyy = this.date.getFullYear();
+		var dd = addZero(this.date.getDate()),
+			mm = addZero(this.date.getMonth() + 1),
+			yyyy = addZero(this.date.getFullYear());
 
-		if (dd < 10) {
-			dd = '0' + dd;
-		}
-
-		if (mm < 10) {
-			mm = '0' + mm;
-		}	
-
-		return dd + "." + mm + "." + yyyy % 1000;
+		return dd + "." + mm + "." + yyyy % 100;
 	}
 
 	this.timeViewFormat = function() {
 
-		var hh = this.date.getHours(),
-			mm = this.date.getMinutes();
-
-		if (hh < 10) {
-			hh = '0' + hh;
-		}
-
-		if (mm < 10) {
-			mm = '0' + mm;
-		}	
+		var hh = addZero(this.date.getHours()),
+			mm = addZero(this.date.getMinutes());
 
 		return hh + ":" + mm;
 	}
 
-	this.idToDate = function(id) {
+	this.idToDate = function(datestr, timestr) {
 
 		//id == doc1_dd-mm-yyyy_thhmm
 
-		var day = id.slice(5, 7),
-			month = id.slice(8, 10),
-			year = id.slice(11, 15),
-			hours = id.slice(17, 19),
-			minutes = id.slice(19),
+		var day = datestr.slice(0, 2),
+			month = datestr.slice(3, 5),
+			year = datestr.slice(5),
+			hours = timestr.slice(1, 3),
+			minutes = timestr.slice(3),
 			set_date = new Date(year, month, day, hours, minutes, 0, 0);
 
 		return set_date;
 	}
 
-	this.getCurrentWeek = function() {
+	this.getCurrentWeek = function(param) {
 
 		this.date.setDate(this.date.getDate() - this.date.getDay());
 
-		var currentWeek = {
+		var key,
+			currentWeek = {
 
 			"sun": 0,
 			"mon": 0,
@@ -104,7 +82,7 @@ function DateEx() {
 
 		for(key in currentWeek) {
 
-			currentWeek[key] = this.dateTranspFormat();
+			currentWeek[key] = (param["transport"] == true) ? this.dateTranspFormat() : this.dateViewFormat();
 			this.date.setDate(this.date.getDate() + 1);
 		}
 
