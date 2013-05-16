e2c1cddc4e0e0ea7315aaf7f007ce88955386f6e
Дима <pandascorp@mail.ru>
2013-05-16 13:14:31 +0300

    resole bag

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,7 @@
 class Ticket < ActiveRecord::Base
-<<<<<<< HEAD
-  attr_accessible :data, :time, :doctor_id, :user_id, :status 
-=======
+
+   attr_accessible :data, :time, :doctor_id, :user_id, :status 
   
-  attr_accessible :data, :time, :doctor_id, :user_id
->>>>>>> 577f1f69801d3368c9088b00cfd8e757314f0bac
    belongs_to :user
    belongs_to :doctor
 end
