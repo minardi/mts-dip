add2b3ecdc09565e5e0423d162ab89effc27c8f6
Svetlana <Linnyk_Svetlana@i.ua>
2013-05-21 12:22:57 +0300

    Fix router

--- app/assets/javascripts/backbone/user_login/user_router.js
+++ app/assets/javascripts/backbone/user_login/user_router.js
@@ -9,31 +9,73 @@
     },
 
     index: function() {
-      $('#current_schedules').addClass("hidden");
-      $('#week_user_tickets').addClass("hidden");
-      $('#next-tickets').addClass("hidden");
-      $('#left_block').removeClass("hidden");
+      // $('#current_schedules').addClass("hidden");
+      // $('#week_user_tickets').addClass("hidden");
+      // $('#next-tickets').addClass("hidden");
+      // $('#left_block').removeClass("hidden");
       // $('#tab_schedules').addClass("hidden");
+      var specializationList,
+          doctorsView,
+          weekly,
+          user,
+          userStatus,
+          dayTimelines,
+          index_elem;
+
+      // index_elem = user && userStatus && specializationList && doctorsView && weekly;
+      // if(index_elem) {
+      //   return index_elem;
+      // } else {
+        user = new UserView({el: $("#login_block")});
+        specializationList = new SpecsView({el:$("#specializations")});
+        doctorsView = new DoctorsView({el:$("#doctors")});
+        weekly = new WeeklyCollectionView({el : $('#weekly-table')});
+        userStatus = new UserStatusView();
+        dayTimelines = new DailySchedulesView({el:$("#daily_schedules")});
+      // }
+      window.UserEx.prototype = user;
+      window.userEx = new window.UserEx();
     },
 
     showHome: function() {
 
-      $('#tab_schedules').addClass("hidden");
-      $('#week_user_tickets').addClass("hidden");
-      $('#next-tickets').removeClass("hidden");
+      var ticketsView;
+
+      // if(ticketsView) {
+      //   return ticketsView;
+      // } else {
+        ticketsView = new TicketsView();
+
+      // }
+      // $('#tab_schedules').addClass("hidden");
+      // $('#week_user_tickets').addClass("hidden");
+      // $('#next-tickets').removeClass("hidden");
       // $('#tab_schedules').addClass("hidden");
 
     },
 
     showPrivateSchedule: function() {
-      $("#home").removeClass("active");
-      $("#private_schedule").addClass("active");
-      $('#left_block').addClass("hidden");
-      $('#weekly-table').addClass("hidden");
-      $('#daily_schedules').addClass("hidden");
-      $('#week_user_tickets').addClass("hidden");
-      $('#current_schedules').removeClass("hidden");
-      $('#week_user_tickets').removeClass("hidden");
+      var currentTimelines,
+          weekdays,
+          prsch_elem;
+
+      // prsch_elem = currentTimelines && weekdays;
+
+      // if(prsch_elem) {
+      //   return prsch_elem;
+      // } else {
+        weekdays = new WeekDaysView({el : $('#week_user_tickets')});    
+        currentTimelines = new CurrentSchedulesView({el:$("#current_schedules")});
+      // }
+
+      // $("#home").removeClass("active");
+      // $("#private_schedule").addClass("active");
+      // $('#left_block').addClass("hidden");
+      // $('#weekly-table').addClass("hidden");
+      // $('#daily_schedules').addClass("hidden");
+      // $('#week_user_tickets').addClass("hidden");
+      // $('#current_schedules').removeClass("hidden");
+      // $('#week_user_tickets').removeClass("hidden");
       // $('#tab_schedules').removeClass("hidden"); 
     }
 
