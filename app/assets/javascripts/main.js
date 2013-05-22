d41ff27c6b57bd2990cd1cf040ed0f2af95ddd92
Glukman <PSGlukman@mail.ru>
2013-05-22 13:36:50 +0300

    someFixRouter

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
-$(function () { 
+
   
-  window.mts = {  specializationList : null,
+  var mts = {  specializationList : null,
                   doctorsView : null,
                   weekly : null,
                   user : null,
@@ -10,27 +10,11 @@ $(function () {
                   currentTimelines : null,
                   weekdays : null,
                   nextTickets : null,
-                  router : new UserRouter()
+                  router : null
                 };
 
-
-  // var specializationList = new SpecsView({el:$("#specializations")}),
-    // var  doctorsView = new DoctorsView({el:$("#doctors")}),
-   // var   dayTimelines = new DailySchedulesView({el:$("#daily_schedules")}),
-
-   //   ticketsView = new TicketsView(),
-   var   nextTickets = new NextTicketsView({el: $("#next-tickets")}),
-         ticketsView = new TicketsView();
-      // weekly = new WeeklyCollectionView({el : $('#weekly-table')}),
-      // weekdays = new WeekDaysView({el : $('#week_user_tickets')}),    
-      // currentTimelines = new CurrentSchedulesView({el:$("#current_schedules")});
-
- //     user = new UserView({el: $("#login_block")}),
-  //    userStatus = new UserStatusView();
-
-      window.router = new UserRouter();
-      // window.UserEx.prototype = user;
-      // window.userEx = new window.UserEx();
+$(function () { 
+  mts.router = new UserRouter();
    Backbone.history.start({pushState: true});
 
 });
