e57eba5f42a656539167eef4d3a7cf29850460fd
pavshulin <pavshulin@gmail.com>
2013-05-31 17:13:25 +0300

    router view & weekly css

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -7,99 +7,24 @@
       'home'                : 'showHome',
       'my-private-schedule' : 'showPrivateSchedule'
     },
-
-    tabNavigator: function(tab) {
-
-      switch(tab) {
-        
-        case 'index' : mts.user = mts.user || new app.UserView({el: $("#login_block")});
-                       
-                       mts.specializationList = mts.specializationList || 
-                                                new app.SpecsView({el:$("#specializations")});
-                       
-                       mts.doctorsView = mts.doctorsView ||
-                                               new app.DoctorsView({el:$("#doctors")});
-                       
-                       mts.weekly = mts.weekly ||
-                                    new app.WeeklyCollectionView({el : $('#weekly-table')});
-                       
-                       mts.userStatus = mts.userStatus || 
-                                             new app.UserStatusView();
-                       
-                       mts.dayTimelines = mts.dayTimelines ||   
-                                                  new app.DailySchedulesView({el:$("#daily_schedules")});
-                       
-                       mts.ticketsView = mts.ticketsView || 
-                                               new app.TicketsView();                                                       
-        break;
-
-        case 'tab1' :  mts.user = mts.user || new app.UserView({el: $("#login_block")});
-                       
-                       mts.specializationList = mts.specializationList || 
-                                                new app.SpecsView({el:$("#specializations")});
-                       
-                       mts.doctorsView = mts.doctorsView ||
-                                               new app.DoctorsView({el:$("#doctors")});
-                       
-                       mts.weekly = mts.weekly ||
-                                    new app.WeeklyCollectionView({el : $('#weekly-table')});
-                       
-                       mts.dayTimelines = mts.dayTimelines ||   
-                                                  new app.DailySchedulesView({el:$("#daily_schedules")});
-                       mts.nextTickets = mts.nextTickets || 
-                                               new app.NextTicketsView({el: $("#next-tickets")});                                                 
-        break;
-
-        case 'tab2' : mts.user = mts.user || new app.UserView({el: $("#login_block")});
-        break;
-      }
-
+    
+    initialize : function() {
+        this.view = new app.RouterView({el : $('#mts-application')});
     },
 
     index: function() {
-
-      this.tabNavigator('index');
-      mts.weekdays = null;
-
-      app.UserEx.prototype = mts.user;
-      app.userEx = new app.UserEx();
-    },
+        
+        this.view.handlerIndex();
+     },
 
     showHome: function() {
 
-      $('#tab2').addClass("hidden");
-      $('#tab1').removeClass("hidden");
-      this.tabNavigator('tab1');
+        this.view.handlerShowHome();
     },
 
     showPrivateSchedule: function() {
-
-      $('#tab1').addClass("hidden");
-      $('#tab2').removeClass("hidden");
-
-      this.tabNavigator('tab2'); 
-
-      if (app.userEx.getRole() === "doctor") {
-
-        if (mts.weekdays instanceof CurrentSchedulesView) {
-          mts.weekdays.refresh();  
-        } else {
-          mts.weekdays = new CurrentSchedulesView({el:$("#current_schedules")});
-        }
-
-        $('#week_user_tickets').addClass("hidden");
-        $('#current_schedules').removeClass("hidden");
-
-      } else {
-        //add same refresh as doctor does) 
-        mts.weekdays = (mts.weekdays instanceof WeekDaysView) ?
-          mts.weekdays : 
-          new WeekDaysView({el : $('#week_user_tickets')});
-
-        $('#week_user_tickets').removeClass("hidden");
-        $('#current_schedules').addClass("hidden");
-      }
-
+        
+        this.view.handlerShowPrivateSchedule();
     }
 
   });
---
+++ app/assets/javascripts/router/config.js
@@ -0,0 +1,3 @@
+//= require ./tab1_template
+//= require ./tab2_template
+//= require ./router_view
