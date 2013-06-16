d027c904a14d62e65a595ebd7e84c56d12d80800
inhuman-en <inhuman.en@gmail.com>
2013-06-16 15:40:41 +0300

    admin user delete

--- app/assets/javascripts/backbone/admin/admin_dashboard_item_view.js
+++ app/assets/javascripts/backbone/admin/admin_dashboard_item_view.js
@@ -59,7 +59,7 @@
 
 		deleteItem: function() {
 			if (confirm("Do you really want to delete this?") === true) {
-				if (this.model instanceof app.UserModel) this.model.switchUrl();
+				if (this.model instanceof app.UserModel) this.model.urlForDelete(this.model.get("id"));
 
 				this.model.destroy();
 			}  
--- app/assets/javascripts/backbone/user_login/user.js
+++ app/assets/javascripts/backbone/user_login/user.js
@@ -15,9 +15,15 @@
 
     url : 'users/login.json',
 
+    //we need to get rid of this bullshit
     switchUrl: function() {
       this.url = "/users";
+    },
+
+    urlForDelete: function(id) {
+      this.url = "/users/" + id;
     }
+
     //,
 
     //validate: function(attrs) {
